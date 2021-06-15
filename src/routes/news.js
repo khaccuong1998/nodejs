1018d9a981da7be4d28b51055d2d1e12e14d524a
khaccuong1998 <khaccuong1998@gmail.com>
2021-06-15 14:35:49 +0700

    up

--- src/routes/news.js
+++ src/routes/news.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const newsController = require('../app/controllers/NewsController');
-router.use(     '/:slug', newsController.show);
+router.use(      '/:slug', newsController.show);
             router.use('/', newsController.index);
 module.exports = router;
